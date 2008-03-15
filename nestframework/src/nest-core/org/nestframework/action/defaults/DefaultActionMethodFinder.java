ea81bd130081e0d3e15d78cb3ee51d881f84edd3
audinchan <audinchan@6698bc5c-722b-0410-8602-6f42757f2e79>
2008-03-15 11:04:56 +0000

    Action和默认Action查找规则，子类优先。

--- nestframework/src/nest-core/org/nestframework/action/defaults/DefaultActionMethodFinder.java
+++ nestframework/src/nest-core/org/nestframework/action/defaults/DefaultActionMethodFinder.java
@@ -2,47 +2,67 @@ package org.nestframework.action.defaults;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Collection;
+import java.util.Set;
 
 import org.nestframework.action.IActionHandler;
 import org.nestframework.annotation.DefaultAction;
 import org.nestframework.annotation.Intercept;
 import org.nestframework.core.ExecuteContext;
 import org.nestframework.core.Stage;
-import org.nestframework.utils.NestUtil;
 
 /**
  * Find action method to be executed.
  * 
  * @author audin
  */
-@Intercept({Stage.INITIAL_ACTIONBEAN})
+@Intercept( { Stage.INITIAL_ACTIONBEAN })
 public class DefaultActionMethodFinder implements IActionHandler {
 
 	public boolean process(ExecuteContext context) throws Exception {
-		Collection<Method> methods = NestUtil.getMethods(context.getActionClass());
+		/*
+		 * 查找规则，子类优先。 
+		 */
+		// Action方法
 		Method actionMethod = null;
+		
+		// 默认的Action方法
 		Method defaultActionMethod = null;
-		for (Method m : methods) {
-			if (!Modifier.isPublic(m.getModifiers())) {
-				continue;
+		
+		// 参数名字集合
+		Set<String> paramNames = context.getParams().keySet();
+		Class<?> clazz = context.getActionClass();
+		while (clazz != null
+				&& (defaultActionMethod == null || actionMethod == null)) {
+			for (Method m1 : clazz.getDeclaredMethods()) {
+				if (!Modifier.isPublic(m1.getModifiers())) {
+					continue;
+				}
+
+				if (defaultActionMethod == null) {
+					DefaultAction defaultAction = m1
+							.getAnnotation(DefaultAction.class);
+					if (defaultAction != null) {
+						defaultActionMethod = m1;
+					}
+				}
+
+				if (actionMethod == null) {
+					if (paramNames.contains(m1.getName())) {
+						actionMethod = m1;
+					}
+				}
 			}
-			DefaultAction defaultAction = m.getAnnotation(DefaultAction.class);
-			if (defaultAction != null) {
-				defaultActionMethod = m; 
-				continue;
-			} else if (context.getParams().keySet().contains(m.getName())) {
-				actionMethod = m;
-				continue;
-			}
+
+			clazz = clazz.getSuperclass();
 		}
+
 		if (actionMethod == null) {
 			actionMethod = defaultActionMethod;
 		}
-		
+
 		context.setAction(actionMethod);
 		context.setDefaultAction(defaultActionMethod);
-		
+
 		return false;
 	}
 }
