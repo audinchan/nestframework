cf9fdc8c266ec9aca1ad9dbaf0d6f3ca4bc4b42c
audinchan <audinchan@6698bc5c-722b-0410-8602-6f42757f2e79>
2008-03-20 10:30:10 +0000

    按新的RuntimeConfiguration重写

--- trunk/nestframework/src/nest-test/org/nestframework/test/TestCaseConfiguration.java
+++ trunk/nestframework/src/nest-test/org/nestframework/test/TestCaseConfiguration.java
@@ -1,24 +1,24 @@
 package org.nestframework.test;
 
+import org.nestframework.action.defaults.ActionMethodAfterInterceptHandler;
+import org.nestframework.action.defaults.ActionMethodBeforeInterceptHandler;
 import org.nestframework.action.defaults.DefaultActionBeanCreater;
 import org.nestframework.action.defaults.DefaultActionBeanSetter;
 import org.nestframework.action.defaults.DefaultActionExecutor;
 import org.nestframework.action.defaults.DefaultActionMethodFinder;
-import org.nestframework.config.IConfiguration;
 import org.nestframework.config.RuntimeConfiguration;
-import org.nestframework.core.NestContext;
 
 public class TestCaseConfiguration extends RuntimeConfiguration {
 	
-	public static IConfiguration getInstance() {
-		IConfiguration nestConfig = new RuntimeConfiguration();
-		nestConfig.addLifecycleHandler(new DefaultActionBeanCreater());
-		nestConfig.addLifecycleHandler(new DefaultActionBeanSetter());
-		nestConfig.addLifecycleHandler(new DefaultActionMethodFinder());
-		nestConfig.addLifecycleHandler(new DefaultActionExecutor());
-		nestConfig.addLifecycleHandler(new TestActionBeanGetter());
-		nestConfig.addLifecycleHandler(new TestActionForwarder());
-		NestContext.setConfig(nestConfig);
-		return nestConfig;
+	@Override
+	protected void addDefaultHandlers() {
+		addLifecycleHandler(new ActionMethodBeforeInterceptHandler());
+		addLifecycleHandler(new DefaultActionBeanCreater());
+		addLifecycleHandler(new DefaultActionBeanSetter());
+		addLifecycleHandler(new DefaultActionMethodFinder());
+		addLifecycleHandler(new DefaultActionExecutor());
+		addLifecycleHandler(new TestActionBeanGetter());
+		addLifecycleHandler(new TestActionForwarder());
+		addLifecycleHandler(new ActionMethodAfterInterceptHandler());
 	}
 }
