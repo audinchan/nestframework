a1f6e77cf986b8f6e5ce2d2103ccf1c50b99b342
AudinChan <AudinChan@6698bc5c-722b-0410-8602-6f42757f2e79>
2008-11-14 09:43:13 +0000

    1.3.6

--- trunk/nestframework/src/nest-core/org/nestframework/utils/NestUtil.java
+++ trunk/nestframework/src/nest-core/org/nestframework/utils/NestUtil.java
@@ -4,8 +4,10 @@ import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.Reader;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -17,6 +19,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import ognl.DefaultMemberAccess;
 import ognl.Ognl;
@@ -321,5 +324,109 @@ public class NestUtil {
 		}
 		int pos = filename.lastIndexOf(File.separatorChar);
 		return filename.substring(pos + 1);
+	}
+	
+	public static void load(Properties props, Reader reader) throws IOException {
+		BufferedReader in = new BufferedReader(reader);
+		while (true) {
+			String line = in.readLine();
+			if (line == null) {
+				return;
+			}
+			line = trimLeadingWhitespace(line);
+			if (line.length() > 0) {
+				char firstChar = line.charAt(0);
+				if (firstChar != '#' && firstChar != '!') {
+					while (endsWithContinuationMarker(line)) {
+						String nextLine = in.readLine();
+						line = line.substring(0, line.length() - 1);
+						if (nextLine != null) {
+							line += trimLeadingWhitespace(nextLine);
+						}
+					}
+					int separatorIndex = line.indexOf("=");
+					if (separatorIndex == -1) {
+						separatorIndex = line.indexOf(":");
+					}
+					String key = (separatorIndex != -1 ? line.substring(0, separatorIndex) : line);
+					String value = (separatorIndex != -1) ? line.substring(separatorIndex + 1) : "";
+					key = trimTrailingWhitespace(key);
+					value = trimLeadingWhitespace(value);
+					props.put(unescape(key), unescape(value));
+				}
+			}
+		}
+	}
+	
+	public static boolean endsWithContinuationMarker(String line) {
+		boolean evenSlashCount = true;
+		int index = line.length() - 1;
+		while (index >= 0 && line.charAt(index) == '\\') {
+			evenSlashCount = !evenSlashCount;
+			index--;
+		}
+		return !evenSlashCount;
+	}
+	
+	public static String unescape(String str) {
+		StringBuffer outBuffer = new StringBuffer(str.length());
+		for (int index = 0; index < str.length();) {
+			char c = str.charAt(index++);
+			if (c == '\\') {
+				c = str.charAt(index++);
+				if (c == 't') {
+					c = '\t';
+				}
+				else if (c == 'r') {
+					c = '\r';
+				}
+				else if (c == 'n') {
+					c = '\n';
+				}
+				else if (c == 'f') {
+					c = '\f';
+				}
+			}
+			outBuffer.append(c);
+		}
+		return outBuffer.toString();
+	}
+	
+	/**
+	 * Trim leading whitespace from the given String.
+	 * @param str the String to check
+	 * @return the trimmed String
+	 * @see java.lang.Character#isWhitespace
+	 */
+	public static String trimLeadingWhitespace(String str) {
+		if (!hasLength(str)) {
+			return str;
+		}
+		StringBuffer buf = new StringBuffer(str);
+		while (buf.length() > 0 && Character.isWhitespace(buf.charAt(0))) {
+			buf.deleteCharAt(0);
+		}
+		return buf.toString();
+	}
+
+	/**
+	 * Trim trailing whitespace from the given String.
+	 * @param str the String to check
+	 * @return the trimmed String
+	 * @see java.lang.Character#isWhitespace
+	 */
+	public static String trimTrailingWhitespace(String str) {
+		if (!hasLength(str)) {
+			return str;
+		}
+		StringBuffer buf = new StringBuffer(str);
+		while (buf.length() > 0 && Character.isWhitespace(buf.charAt(buf.length() - 1))) {
+			buf.deleteCharAt(buf.length() - 1);
+		}
+		return buf.toString();
+	}
+	
+	public static boolean hasLength(String str) {
+		return (str != null && str.length() > 0);
 	}
 }
